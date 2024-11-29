25fcbe7d94a789e0e7ac85ea5d5f8c77e4157d91
ErnestGeo24 <ernest1.avasi@gmail.com>
2024-11-29 09:47:46 +0000

    finito

--- client-angular/src/app/app.component.ts
+++ client-angular/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http'; //HTTP CLIENT
-
 @Component({
 selector: 'app-root',
 templateUrl: './app.component.html',
