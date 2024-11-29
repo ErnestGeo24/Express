28dfec77de690e7095d7b4eea77fcf880f46d8aa
ErnestGeo24 <ernest1.avasi@gmail.com>
2024-11-29 09:39:16 +0000

    finito

--- client-angular/src/app/app.component.ts
+++ client-angular/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'; //HTTP CLIENT
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client-angular';
+export class AppComponent implements OnInit{
+results: string[];
+// Inject HttpClient into your component or service.
+constructor(private http: HttpClient) {}
+ngOnInit(): void {
+// Make the HTTP request:
+this.http.get('http://GITPOD_SERVER_URL/api').subscribe(data => {
+// Read the result field from the JSON response.
+this.results = data['results'];
+});
+}
 }
